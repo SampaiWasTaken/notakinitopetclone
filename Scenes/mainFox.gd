561407b31c785c2869131077bc852d8ff090f4dc
MrMerlinH <67464744+MrMerlinH@users.noreply.github.com>
2025-02-15 16:35:32 +0100

    merged stuff
    i like git

---
+++ Scenes/mainFox.gd
@@ -0,0 +1,16 @@
+extends Window
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _on_control_pet_btn_pressed() -> void:
+	$Fox.play("pet")
+
+func _on_fox_animation_finished() -> void:
+	$Fox.play("idle")
