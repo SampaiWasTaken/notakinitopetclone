293a4ee6320debe7fd21daebf23efeaea495297b
MrMerlinH <67464744+MrMerlinH@users.noreply.github.com>
2025-02-19 18:17:42 +0100

    no

--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -9,6 +9,28 @@ var food = 50
 var fun = 50
 var love = 50
 
+@export var min_decrease: int = 1  # Minimum amount to decrease
+@export var max_decrease: int = 5 # Maximum amount to decrease
+@export var interval: float = 180 # Time in seconds (3 minutes)
+
+
+func _ready():
+	var timer = Timer.new()
+	timer.wait_time = interval
+	timer.autostart = true
+	timer.one_shot = false
+	timer.timeout.connect(_on_timer_timeout)
+	add_child(timer)
+
+func _on_timer_timeout():
+	var decrease_amount1 = randi_range(min_decrease, max_decrease)
+	var decrease_amount2 = randi_range(min_decrease, max_decrease)
+	var decrease_amount3 = randi_range(min_decrease, max_decrease)
+	addFood(decrease_amount1*-1)
+	addFun(decrease_amount2*-1)
+	addLove(decrease_amount3*-1)
+	
+
 func addFood(amount:float):
 	food = food + amount
 	if food > 100:
