26cb84638f6f00939e102ca986ff6500891a9e50
MrMerlinH <67464744+MrMerlinH@users.noreply.github.com>
2025-02-19 15:57:17 +0100

    added globals, love, fun, food

---
+++ Scripts/globals.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+#global Variables 
+var food = 50
+var fun = 50
+var love = 50
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
