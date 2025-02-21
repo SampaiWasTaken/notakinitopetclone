fcdf29f517019ce4a5c8cf348b536c55557d8c4c
SampaiWasTaken <samuel.radler@htl-klu.at>
2025-02-21 10:47:20 +0100

    randomize events

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -3,6 +3,7 @@ extends Node
 var file_path = "user://hello.txt"  # This will save in the game's user directory
 var file = FileAccess.open(file_path, FileAccess.WRITE)
 var executed = false
+var picks = [0, 1, 2, 3]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
@@ -28,14 +29,24 @@ func cmdTest():
 	OS.execute("cmd.exe", ["/C", "start", "cmd", "/C", "echo I'm inside your walls && pause"], [], false, true)
 
 func creepy():
-	await get_tree().create_timer(randf_range(5, 5)).timeout  
-	var thr = Thread.new()
-	thr.start(cmdTest)
-	$"../InsideTheWalls".play()
-	#thr.wait_to_finish()
-	await get_tree().create_timer(randf_range(5, 5)).timeout  
-	OS.execute("explorer", ["https://www.yyyyyyy.info/"], ["--new-window"], false, true)
-	await get_tree().create_timer(randf_range(5, 5)).timeout  
-	OS.alert("Are you sure you're alone?", "Hello?")
-	await get_tree().create_timer(randf_range(5, 5)).timeout
-	notepadNote()
+	await get_tree().create_timer(randf_range(25, 50)).timeout  
+	randomizeEvents()
+
+func randomizeEvents():
+	if picks.is_empty():
+		pass
+	var picked = picks.pick_random()
+	picks.erase(picked)
+	
+	match picked:
+		0:
+			var thr = Thread.new()
+			thr.start(cmdTest)
+			$"../InsideTheWalls".play()
+		1:
+			OS.execute("explorer", ["https://www.yyyyyyy.info/"], ["--new-window"], false, true)
+		2:
+			OS.alert("Are you sure you're alone?", "Hello?")
+		3:
+			notepadNote()
+	pass
